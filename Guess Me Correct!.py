13c1edde59b42dd926fa7e0c72588086c7bdc231
Sai07joker <59677253+Sai07joker@users.noreply.github.com>
2020-01-10 01:15:47 -0800

    added image

--- Guess Me Correct!.py
+++ Guess Me Correct!.py
@@ -1,6 +1,7 @@
 #This is a guess the number game.
 import random
 from playsound import playsound
+from PIL import Image
 
 guessesTaken = 0
 
@@ -16,7 +17,7 @@ while guessesTaken < 5:
 
      guess = int(guess)
      guessesTaken = guessesTaken + 1
-     
+
      if guess < number:
 
          print('Your guess is too low.') # There are eight spaces in front of print.
@@ -43,4 +44,5 @@ if guess != number:
       number = str(number)
       print('Nope. The number I was thinking of was ' + number)
       playsound("oh_no.mp3")
-      
+      myImage = Image.open("oh_no.jpg");
+      myImage.show();
